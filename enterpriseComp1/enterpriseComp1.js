255d9405746ee3b4d0af919eb047edd1a53c9b3c
ss10del <ss10@deloitte.com>
2026-04-08 15:29:11 +0530

    Create enterpriseComp1 component with computed property

---
+++ enterpriseComp1/enterpriseComp1.js
@@ -0,0 +1,10 @@
+
+import { LightningElement } from 'lwc';
+
+export default class enterpriseComp1 extends LightningElement {
+    message = 'V1 message';
+
+    get computed() {
+        return this.message.toUpperCase();
+    }
+}
