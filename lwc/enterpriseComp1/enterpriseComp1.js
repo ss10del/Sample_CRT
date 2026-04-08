1afe5b220dc2d26538113ba0bc633871fa600c8b
ss10del <ss10@deloitte.com>
2026-04-08 17:18:28 +0530

    Update enterpriseComp1.js

--- lwc/enterpriseComp1/enterpriseComp1.js
+++ lwc/enterpriseComp1/enterpriseComp1.js
@@ -2,7 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class enterpriseComp1 extends LightningElement {
     message = 'V1 message';
-
+//123
     get computed() {
         return this.message.toUpperCase();
     }
